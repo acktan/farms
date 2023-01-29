02abb5c0f3e16d78d32c8bafb5cfaf985e52c708
acktan <94125871+acktan@users.noreply.github.com>
2023-01-29 12:27:42 +0100

    inference predict future

--- src/inference/inference.py
+++ src/inference/inference.py
@@ -9,21 +9,15 @@ class Inference():
                  model,
                  train_data,
                  future_data) -> None:
+        """Infer model performance and predict future data
+        """
         self.config = config
         self.model = model
         self.train_data = train_data
         self.future_data = future_data
         
-    def get_feature_importance(self):
-        dateCols = self.train_data[0:2]
-        plt.style.use('default')
-        inputCols = self.train_data.columns[2:18].tolist() + dateCols
-        plt.figure().set_size_inches(16.5, 8.5)
-        plt.bar(range(len(inputCols)), self.model.named_steps['extratreesregressor'].feature_importances_)
-        plt.title('Feature importance')
-        plt.xticks(range(len(inputCols)), inputCols, rotation='vertical')
-        plt.xlabel("Feature")
-        plt.ylabel("Importance")
-        plt.show()
+    def predict_future(self):
+        yhat = self.model.predict(self.future_data)
+        return yhat
+    
         
-    def 
